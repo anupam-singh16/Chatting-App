812dec60ee7f3bfe4ebb92fe7007ca450248657b
anupam singh <anupamsingh16june@gmail.com>
2023-10-18 16:46:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Room from "./createRoom"
+
+const App = () => {
+  return (
+    <div><Room/></div>
+  )
+}
+
+export default App
---
+++ src/createRoom.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+  const [valid,setValid] = useState(null)
+
+  const joinRoom = () => {
+    // setValid(false)
+    if (username !== "" ) {
+      setValid(false);
+      if(room !== ""){
+      socket.emit("join_room", room);
+      setShowChat(true);
+      setValid(true);
+      }
+    }else{
+      setValid(true)
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Can We Chat</h3>
+          <input
+            type="text"
+            placeholder="Name*"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          {valid === true?"please fill name ":""}
+          <input
+            type="text"
+            placeholder="Room ID*"
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          {valid === false ?"please enter room id":""}
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default App;
